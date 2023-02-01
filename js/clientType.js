5d0d1e84098376a0342e4d2724a258f507522544
Ahmed <ahmedaraby01023980886@gmail.com>
2023-02-01 12:07:09 +0200

    checking bug

--- js/clientType.js
+++ js/clientType.js
@@ -4,11 +4,13 @@ const clientsInfo = document.getElementsByClassName("clients-info");
 select.onchange = e => {
     if (e.target.value === "person") {
         for (let i = 0; i < clientsInfo.length; i++) {
-            clientsInfo[i].style.display = "none"
+            clientsInfo[i].style.display = "none";
+            alert("Person is Checked");
         }
     } else {
         for (let i = 0; i < clientsInfo.length; i++) {
             clientsInfo[i].style.display = "block";
+            alert("Company is Checked");
         }
     }
 }
